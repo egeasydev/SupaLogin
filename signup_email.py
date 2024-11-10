3e96861713d4cf34ad10ba9e7389f249c1aae274
egeasydev <egeasydev@gmail.com>
2024-11-10 22:34:48 +0900

    [init] first init

--- signup_email.py
+++ signup_email.py
@@ -1,6 +1,9 @@
 import os
 from supabase import create_client, Client
 import streamlit as st
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(), override=True)
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_KEY")
