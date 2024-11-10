29f97793e77eb49c5a284e03439a720b293d30d1
egeasydev <egeasydev@gmail.com>
2024-11-10 22:22:14 +0900

    [init] first init

---
+++ signup_email.py
@@ -0,0 +1,16 @@
+import os
+from supabase import create_client, Client
+import streamlit as st
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+email = st.text_input("email")
+password = st.text_input("password")
+
+if st.button("Submit"):
+    response = supabase.auth.sign_up(
+        {"email": email, "password": password}
+    )
+
